638989d773eccebe9aeee6f39fea23bf9fa28c30
Claus Kühnel <info@ckuehnel.ch>
2016-12-03 19:29:28 +0100

    Add files via upload
    Read date & time from Google server

---
+++ getDateTime.py
@@ -0,0 +1,37 @@
+# Title: Google Time
+# Date: 2016-12-03
+# Id : getDateTime.py
+# Firmware: 0.9.6.b1
+# Copyright: Claus Kuehnel  (ckuehnel.sz@gmail.com)
+
+# Read date & time from Google server
+
+import socket
+addr = socket.getaddrinfo('www.google.ch', 80)[0][-1]
+
+debug = False
+
+s = socket.socket()
+s.connect(addr)
+s.send(b'GET / HTTP/1.1\r\nHost: www.google.ch\n\r\n')
+data = s.recv(1000)
+
+if '200 OK' in data:
+    print('OK')
+else:
+    print('Socket error')
+    
+data = data.decode()
+if debug: print(data)
+
+str = 'Date: '
+start = data.find(str)
+stop = data.find('\r\n', start)
+
+date = data[start+len(str):stop]
+
+if debug :  print(start,  stop)
+    
+print (date)
+
+s.close()
