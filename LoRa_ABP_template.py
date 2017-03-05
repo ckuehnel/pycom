10cbc10b6c3b6aa8570af0b32337b8e88bc34f07
Claus Kühnel <info@ckuehnel.ch>
2017-03-05 17:20:21 +0100

    Add files via upload
    LoRaWAN example with Pycom's LoPy
    This is a templete - you have to add WLAN credentials and LoRaWAN access data (DevAddr, NWS Key, App Key)

---
+++ LoRa_ABP_template.py
@@ -0,0 +1,71 @@
+import network
+from network import WLAN
+from network import LoRa
+import socket
+import binascii
+import struct
+import pycom
+import utime   
+import machine
+import time
+
+# Credentials for WLAN
+SSID = '<Your SSID>'
+KEY  = '<Your Password>'
+
+# setup as a station
+wlan = network.WLAN(mode=WLAN.STA)
+wlan.connect(SSID, auth=(WLAN.WPA2,KEY))
+
+print("Try to connect...")
+while not wlan.isconnected():
+    time.sleep_ms(100)
+    print(".",  end="")
+
+print("")   
+print("Connected.")
+print('IP address:', wlan.ifconfig()[0])
+
+pycom.heartbeat(False)
+
+adc = machine.ADC()               # create an ADC object
+apin = adc.channel(pin='P16')   # create an analog pin on P16 & connect TMP36
+
+# Initialize LoRa in LORAWAN mode.
+lora = LoRa(mode=LoRa.LORAWAN)
+
+# create an ABP authentication params
+dev_addr = struct.unpack(">l", binascii.unhexlify('<Your DevAddr>'.replace(' ','')))[0]
+nwk_swkey = binascii.unhexlify('<Your network key>'.replace(' ',''))
+app_swkey = binascii.unhexlify('<Yout app key'.replace(' ',''))
+
+# join a network using ABP (Activation By Personalization)
+lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
+
+while not lora.has_joined():
+    print('Trying to join LoRaWAN network')
+    utime.sleep(1)
+    pass
+print('Joined LoRaWAN Network')
+
+pycom.rgbled(0x00ff00)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+count=0
+
+while True:
+    value = apin()
+
+    # LoPy  has 1.1 V input range for ADC
+    temp = ((value * 1100 ) / 1024 - 500) / 100
+    print("Temperature = %5.1f *C" % (temp))
+    print('Sending Packet')
+    msg = repr(round(temp, 1)) + ' *C'''
+    #print(msg)
+    s.send(msg)
+    count = count+1
+    print('Done sending - %d' %(count))
+    pycom.rgbled(0x00ffff)
+    utime.sleep_ms(10)
+    pycom.rgbled(0x00000)
+    utime.sleep(60)
